84e2d5fa97dc69df9f53bd4d503b0610600dc0ed
Maikel Gomez <156638775+MaiGdev@users.noreply.github.com>
2025-02-20 12:17:33 -0600

    Update index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,5 @@
 export * from "./useAuthStore";
 export * from "./useForm";
 export * from "./useScheduleLogic";
-export * from "./useScheduleState";
 export * from "./useUserStore";
 export * from "./useScheduleAppointmentApi";
